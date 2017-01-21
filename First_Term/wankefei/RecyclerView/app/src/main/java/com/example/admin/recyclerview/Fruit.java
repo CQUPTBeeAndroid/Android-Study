739d2d1d18f0e3b84208d97f9fdfeb2802b74721
Desirered <990802860@qq.com>
2017-01-21 18:20:16 +0800

    first demo

---
+++ First_Term/wankefei/RecyclerView/app/src/main/java/com/example/admin/recyclerview/Fruit.java
@@ -0,0 +1,17 @@
+package com.example.admin.recyclerview;
+
+public class Fruit{
+    private String name;
+    private int imageId;
+    public Fruit(String name,int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
